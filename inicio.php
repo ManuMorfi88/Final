f8f359e6da71a16e0cda6396021fa663f2c319a3
Janeth <al21020025@itsa.edu.mx>
2024-06-18 13:58:57 -0600

    holi

--- inicio.php
+++ inicio.php
@@ -16,6 +16,8 @@ $email_usuario = isset($_SESSION['email']) ? $_SESSION['email'] : null;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="inicio.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
 </head>
 <body>
     <header>
--- loginhtml.php
+++ loginhtml.php
@@ -9,13 +9,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="datos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
 </head>
 <body>
     <header>
         <h1 class="n-sitio">PANADERÍA RODRÍGUEZ</h1>
     </header>
     
-    <h1>INICIAR SESIÓN</h1>
+    <h1 class="sesion">INICIAR SESIÓN</h1>
 
     <section>
         <div class="contenedor-datos">
